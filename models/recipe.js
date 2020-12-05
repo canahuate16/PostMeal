e6adb8e8500c135e8d864da0f99905179d0ec991
Luis Canahuate <luis.canahuate16@gmail.com>
2020-12-05 15:21:26 -0500

    added landing page and functionality to allow user to share meals

--- models/recipe.js
+++ models/recipe.js
@@ -1,22 +1,19 @@
+const { DATE } = require("sequelize");
+
 module.exports = function(sequelize, DataTypes) {
   const Recipe = sequelize.define("Recipe", {
-    title: {
+    author: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        len: [1]
-      }
+    },
+    
+    recipe: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     body: {
       type: DataTypes.TEXT,
       allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    category: {
-      type: DataTypes.STRING,
-      defaultValue: "Personal"
     }
   });
   return Recipe;
---
+++ public/js/recipe.js
@@ -0,0 +1,59 @@
+// When the page loads, grab and display all of our chirps
+$.get("/api/all", function (data) {
+
+    if (data.length !== 0) {
+
+        for (var i = 0; i < data.length; i++) {
+
+            var row = $("<div>");
+            row.addClass("chirp");
+
+            row.append("<p>" + data[i].author + " shared.. </p>");
+            row.append("<p>" + data[i].recipe + "</p>");
+            row.append("<p>" + data[i].body + "</p>");
+            row.append("<p>On " + new Date(data[i].created_at).toLocaleDateString() + "</p>");
+
+            $("#chirp-area").prepend(row);
+
+        }
+
+    }
+
+});
+
+// When user chirps (clicks addBtn)
+$("#chirp-submit").on("click", function (event) {
+    event.preventDefault();
+    
+    // Make a newChirp object
+    var newChirp = {
+        author: $("#author").val().trim(),
+        recipe: $("#recipe").val().trim(),
+        body: $("#chirp-box").val().trim(),
+        created_at: new Date().toISOString().slice(0, 19).replace('T', ' ')
+    };
+
+    console.log(newChirp);
+
+    // Send an AJAX POST-request with jQuery
+    $.post("/api/recipe", newChirp)
+        // On success, run the following code
+        .then(function () {
+
+            var row = $("<div>");
+            row.addClass("chirp");
+
+            row.append("<p>" + newChirp.author + " shared: </p>");
+            row.append("<p>" + newChirp.recipe + "</p>");
+            row.append("<p>" + newChirp.body + "</p>");
+            row.append("<p>On " + new Date(newChirp.created_at).toLocaleDateString() + "</p>");
+
+            $("#chirp-area").prepend(row);
+
+        });
+
+    // Empty each input box by replacing the value with an empty string
+    $("#author").val("");
+    $("#recipe").val("");
+    $("#chirp-box").val("");
+});
